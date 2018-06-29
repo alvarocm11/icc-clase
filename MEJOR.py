dc7187b5209feed086071b3db1247713833aa151
alvarocm11 <40642098+alvarocm11@users.noreply.github.com>
2018-06-29 10:50:11 -0500

    Parte Alvaro
    Yo hice la altura maxima ahora solo falta el recorrido en el eje horizontal

--- MEJOR.py
+++ MEJOR.py
@@ -5,5 +5,7 @@ theta=Angulo*pi/180
 g=9.81
 
 tiempo=(2*V_Inicial*sin(theta))/g
+alturamax=((V_Inicial**2)*(sin(theta)**2))/2*g
 
-print("Tiempo total de vuelo:", tiempo)
+print("Tiempo total de vuelo:", tiempo,"s")
+print("La altura maxima es:",alturamax,"m")
