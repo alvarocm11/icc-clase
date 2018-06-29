bd92ccdc5a69d4085d6e19bc3a7b4bd06a02a6e2
ABallon00 <40697430+ABallon00@users.noreply.github.com>
2018-06-29 10:36:12 -0500

    Parte Ballón
    falta altura maxima y recorrido en el eje horizontal

--- MEJOR.py
+++ MEJOR.py
@@ -1,1 +1,9 @@
-awdawdawdawdsss
+V_Inicial = float(input("Ingrese la velocidad inicial:"))
+Angulo = float(input("Ingrese el angulo al que se va a lanzar el proyectil:"))
+
+theta=Angulo*pi/180
+g=9.81
+
+tiempo=(2*V_Inicial*sin(theta))/g
+
+print("Tiempo total de vuelo:", tiempo)
