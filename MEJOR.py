70f1880e774fc9965decf883397374fd0f4f25b6
alvarocm11 <40642098+alvarocm11@users.noreply.github.com>
2018-07-02 09:46:04 -0500

    alvaro
    final

--- MEJOR.py
+++ MEJOR.py
@@ -6,7 +6,7 @@ g=9.81
 
 tiempo=(2*V_Inicial*sin(theta))/g
 alturamax=((V_Inicial**2)*(sin(theta)**2))/2*g
-distance=(V_inicial*cos(theta))*time
+distance=(V_Inicial*cos(theta))*time
 
 print("Tiempo total de vuelo:", tiempo,"s")
 print("La altura maxima es:",alturamax,"m")
